87cc3f16085276e6cf6f08757b5b726a12acfcf6
Lucien Lafaye <lafaye@et.intechinfo.fr>
2024-05-17 16:40:00 +0200

    First commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.esiea.spotyMike4Real',
+  appName: 'SpotyMike',
+  webDir: 'www'
+};
+
+export default config;
---
+++ src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
