d43764204991ae7f6201d166ee9bdc6dc4aedd81
Clement Haytham <116069043+hxthm@users.noreply.github.com>
2024-05-20 00:36:32 +0200

    Navbar done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonIcon, IonTabBar } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonTabBar, IonIcon, IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() {}
