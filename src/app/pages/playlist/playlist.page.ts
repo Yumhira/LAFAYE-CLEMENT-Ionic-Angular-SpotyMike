334e116f7915fae888ac19f67894784a7c8b1600
Lucien Lafaye <lafaye@et.intechinfo.fr>
2024-05-30 13:25:03 +0200

    Add playlist page

--- src/app/pages/playlist/playlist.page.ts
+++ src/app/pages/playlist/playlist.page.ts
@@ -1,20 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonIcon, IonButton } from '@ionic/angular/standalone';
+import { Location } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { chevronBack, ellipsisHorizontal } from 'ionicons/icons';
 
 @Component({
   selector: 'app-playlist',
   templateUrl: './playlist.page.html',
   styleUrls: ['./playlist.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonIcon, IonButton, IonTitle, IonCol, IonRow, IonToolbar, CommonModule, FormsModule]
 })
 export class PlaylistPage implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) {
+    addIcons({ chevronBack, ellipsisHorizontal })
+  }
 
   ngOnInit() {
+  }
+
+  backClicked() {
+    this._location.back();
   }
 
 }
