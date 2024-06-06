9effbdd2f2c482402bcbbbec6a6fa47a0a4d485e
Clement Haytham <116069043+hxthm@users.noreply.github.com>
2024-06-06 22:16:41 +0200

    get album in home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,11 +14,12 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { arrowForwardOutline, searchOutline, ellipsisHorizontal } from 'ionicons/icons';
+import { IAlbum } from 'src/app/core/interfaces/album';
 import { IUserTest } from 'src/app/core/interfaces/userTest';
 import { PhoneNumberPipe } from 'src/app/core/pipe/phone-number.pipe';
+import { FirestoreService } from 'src/app/core/services/firestore.service';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { UserService } from 'src/app/core/services/user.service';
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -35,12 +36,15 @@ import { UserService } from 'src/app/core/services/user.service';
     IonButtons,
     IonRow,
     IonCol,
-    IonCard
+    IonCard,
   ],
 })
 export class HomePage {
   users: IUserTest[] = [];
+  albums: any[] = [];
+  user: any;
   private userService = inject(UserService);
+  private fireStoreService = inject(FirestoreService);
   private local = inject(LocalStorageService);
 
   ngOnInit() {
@@ -52,13 +56,19 @@ export class HomePage {
     //     this.local.setElement('users', JSON.stringify(this.users));
     //   });
     // else this.users = users;
+    this.getAlbum();
   }
 
   constructor(private router: Router) {
-    addIcons({arrowForwardOutline, searchOutline, ellipsisHorizontal});
+    addIcons({ arrowForwardOutline, searchOutline, ellipsisHorizontal });
   }
 
-  goToPlayer(){
+  goToPlayer() {
     this.router.navigate(['/player']);
+  }
+
+  async getAlbum() {
+    this.albums = await this.fireStoreService.getAlbum();
+    console.log(this.albums);
   }
 }
