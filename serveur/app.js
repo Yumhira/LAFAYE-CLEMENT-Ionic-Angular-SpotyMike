c7abfd78c88dc77387659de7b0cbaa85778cf995
Clement Haytham <116069043+hxthm@users.noreply.github.com>
2024-06-18 14:23:29 +0200

    Fix start script and dependencies

--- serveur/app.js
+++ serveur/app.js
@@ -5,36 +5,33 @@ const path = require('path');
 const app = express();
 const PORT = 3001;
 
-app.get('/stream', (req, res) => {
-
-  const filePatch = path.resolve(__dirname, 'logobi.mp3');
-  const info = fs.statSync(filePatch);
+app.get("http://spotymikelh.eu-4.evennode.com/", (req, res) => {
+  const filePath = path.resolve(__dirname, "logobi.mp3");
+  const info = fs.statSync(filePath);
   const fileSize = info.size;
   const range = req.headers.range;
-  if(range) {
-    const parts = range.replace(/bytes=/, '').split('-');
-  const start = parseInt(parts[0], 10);
-  const end = parts[1]? parseInt(parts[1], 10) : fileSize - 1;
-  const chunksize = (end - start) + 1;
-  const file = fs.createReadStream(filePath, {start, end});
-  const head = {
-    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-    'Accept-Ranges': 'bytes',
-    'Content-Length': chunksize,
-    'Content-Type': 'audio/mpeg',
-  };
-  res.writeHead(head);
-  file.pipe(res);
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(filePath, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "audio/mpeg",
+    };
+    res.writeHead(head);
+    file.pipe(res);
   } else {
     const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'audio/mpeg',
+      "Content-Length": fileSize,
+      "Content-Type": "audio/mpeg",
     };
     res.writeHead(200, head);
     fs.createReadStream(filePath).pipe(res);
   }
-
-
 });
 
 app.listen(PORT, () => {
