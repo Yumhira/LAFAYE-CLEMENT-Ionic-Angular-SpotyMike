9e53db551bdf498e3af8d4617da1467ab35a86bb
Clement Haytham <116069043+hxthm@users.noreply.github.com>
2024-06-21 15:01:00 +0200

    Header segment artist + fix tabs

--- src/app/pages/account/fragments/artiste/artiste.component.ts
+++ src/app/pages/account/fragments/artiste/artiste.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FirestoreService } from 'src/app/core/services/firestore.service';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonInput,
+  IonTabButton,
+  IonItem,
+  IonLabel,
+  IonButton,
+  IonSegmentButton,
+  IonSegment,
+} from '@ionic/angular/standalone';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-artiste',
   templateUrl: './artiste.component.html',
   styleUrls: ['./artiste.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonSegment, IonSegmentButton, IonButton, IonLabel, IonItem, IonTabButton, IonInput, IonCol, IonRow, IonGrid, IonContent, IonHeader, IonTitle, IonToolbar, FormsModule, ReactiveFormsModule],
 })
 export class ArtisteComponent implements OnInit {
+  private fireStoreService = inject(FirestoreService);
+
+  user: any[] = [];
+  segmentArtist = 'Compte';
   constructor() {}
 
   ngOnInit() {}
