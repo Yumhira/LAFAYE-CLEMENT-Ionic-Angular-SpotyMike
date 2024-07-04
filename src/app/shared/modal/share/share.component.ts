9d1462a2c9b3a486df7c3d8caa81061221a579b7
Lucien Lafaye <lafaye@et.intechinfo.fr>
2024-07-04 17:09:41 +0200

    New share modal

--- src/app/shared/modal/share/share.component.ts
+++ src/app/shared/modal/share/share.component.ts
@@ -8,7 +8,10 @@ import {
   IonItem,
   IonList,
   IonLabel,
+  IonIcon
 } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-share',
@@ -16,17 +19,37 @@ import {
   styleUrls: ['./share.component.scss'],
   standalone: true,
   imports: [
-    IonItem,
+  IonItem,
     IonContent,
     IonTitle,
     IonButton,
     IonButtons,
     IonList,
     IonLabel,
+    IonIcon
   ],
 })
 export class ShareComponent implements OnInit {
-  constructor(private modalController: ModalController) {}
+  
+  constructor(private modalController: ModalController) {
+    addIcons({ closeOutline });
+  }
 
   ngOnInit() {}
+
+  backClicked() {
+    this.modalController.dismiss();
+  }
+
+  shareFacebook() {
+    window.location.href ='https://www.facebook.com/';
+  }
+
+  shareTwitter(){
+    window.location.href = 'https://twitter.com/';
+  }
+
+  shareInstagram(){
+    window.location.href = 'https://instagram.com/';
+  }
 }
